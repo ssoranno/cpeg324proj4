37eb03950bee319af3916ecffcddde20e2c4da0a
ssoranno <ssoranno363@gmail.com>
2018-05-09 16:54:02 -0400

    regs

--- exe_wb.vhdl
+++ exe_wb.vhdl
@@ -2,13 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 -- main entity
-entity id_exe is
+entity exe_wb is
 	port (
-	rd : in std_logic_vector(1 downto 0); -- register destination binary value
+	inTemp: in std_logic;
+	ALUO: in std_logic_vector(7 downto 0);
 	imm : in std_logic_vector(7 downto 0);
-	rego1 : in std_logic_vector(7 downto 0);
-	rego2 : in std_logic_vector(7 downto 0);
-	skipval : in std_logic_vector(3 downto 0);
 	cs1: in std_logic;
 	cs2: in std_logic;
 	cs3: in std_logic;
@@ -17,20 +15,21 @@ entity id_exe is
 	cs6: in std_logic;
 	clk : in std_logic; -- clock input
 	enable : in std_logic; -- enable input
-	ord : out std_logic_vector(1 downto 0); -- output 1 for ALU
+	rd : in std_logic_vector(1 downto 0); -- register destination binary value
+	oinTemp: out std_logic;
+	oALUO: out std_logic_vector(7 downto 0);
 	oimm : out std_logic_vector(7 downto 0); -- output 2 for ALU
-	o1 : out std_logic_vector(7 downto 0); -- output 2 for ALU
-	o2 : out std_logic_vector(7 downto 0); -- output 2 for ALU
-	oskipval: out std_logic_vector(3 downto 0);
-	ocs1 : out std_logic; -- output 2 for ALU
-	ocs2 : out std_logic;
-	ocs3 : out std_logic;
+	ocs1: out std_logic;
+	ocs2: out std_logic;
+	ocs3: out std_logic;
 	ocs4 : out std_logic;
 	ocs5 : out std_logic;
-	ocs6 : out std_logic);
-end id_exe;
+	ocs6 : out std_logic;
+	ord : out std_logic_vector(1 downto 0) -- output 1 for ALU
+	);
+end exe_wb;
 
-architecture reg of id_exe is
+architecture reg of exe_wb is
 -- compont for each of the register flipflops
 component flipFlop_8bit is	
 	port(
@@ -66,11 +65,10 @@ end component;
 
 begin
 
-RDest: flipFlop_2bit port map(d=>rd, clk=>clk, enable=>enable, q=>ord);
+IT: flipFlop_bit port map(d=>inTemp, clk=>clk, enable=>enable, q=>oinTemp);
+ALU: flipFlop_8bit port map(d=>ALUO, clk=>clk, enable=>enable, q=>oALUO);
+FFRD: flipFlop_2bit port map(d=>rd, clk=>clk, enable=>enable, q=>ord);
 IMME: flipFlop_8bit port map(d=>imm, clk=>clk, enable=>enable, q=>oimm);
-Out1: flipFlop_8bit port map(d=>rego1, clk=>clk, enable=>enable, q=>o1);
-Out2: flipFlop_8bit port map(d=>rego2, clk=>clk, enable=>enable, q=>o2);
-SV: flipFlop_4bit port map(d=>skipval, clk=>clk, enable=>enable, q=>oskipval);
 C1: flipFlop_bit port map(d=>cs1, clk=>clk, enable=>enable, q=>ocs1);
 C2: flipFlop_bit port map(d=>cs2, clk=>clk, enable=>enable, q=>ocs2);
 C3: flipFlop_bit port map(d=>cs3, clk=>clk, enable=>enable, q=>ocs3);
